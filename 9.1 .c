fb14d1fb6beb01eaafbc03626a21b691be877f91
Jakaria Hossin <jakariahossinsv@gmail.com>
2024-10-26 20:47:04 +0600

    Create 9.1 .c

---
+++ 9.1 .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,fact=1;
+    printf("Enter a value:");
+    scanf("%d",&n);
+    {
+        fact= fact*1;
+    }
+    printf("Factorial=%d",fact);
+    return 0;
+}
