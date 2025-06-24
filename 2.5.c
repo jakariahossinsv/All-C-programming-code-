2ba5f623f6b592485e8800e20597101f6ea8a522
Jakaria Hossin <jakariahossinsv@gmail.com>
2025-06-24 15:45:44 +0600

    Create 2.5.c

---
+++ 2.5.c
@@ -0,0 +1,10 @@
+# include <stdio .h>
+int main()
+{
+  int num1,num2,div;
+  scanf("%f ", & num1);
+  scanf("%f ",  & num2);
+  div= num1 / num2 ;
+  peintf ("%.0f/ %.0f = %.2f\n",num1,num2,div);
+  return 0;
+}
