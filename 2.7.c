ccdae9acbeede5eff3d90ac60b53327ca663ead6
Jakaria Hossin <jakariahossinsv@gmail.com>
2025-06-24 15:55:19 +0600

    Create 2.7.c

---
+++ 2.7.c
@@ -0,0 +1,22 @@
+# include <stdio .h>
+int main()
+{
+  float radius,area;
+  scanf("%f ", & radius);
+  area= 3.1416 * radus * radius ;
+  peintf ("area = %.2f\n",area);
+  return 0;
+}
+
+/* or
+
+# include <stdio .h>
+# define PI 3.1416
+int main()
+{
+  float radius,area;
+  scanf("%f ", & radius);
+  area= PI * radus * radius ;
+  peintf ("area = %.2f\n",area);
+  return 0;
+}
