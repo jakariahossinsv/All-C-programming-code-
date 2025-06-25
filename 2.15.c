3634248a3a9ba1fdd9944ad58886a7d957ced298
Jakaria Hossin <jakariahossinsv@gmail.com>
2025-06-25 19:46:28 +0600

    Create 2.15.c

---
+++ 2.15.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter thhe first number : ");
+scnf("%d",&a);
+  printf("Enter second number: ")
+scnf("%d",&b);
+  c= a^b
+peintf("Bitwise exclusive or result :  %d\n",c);
+}
