43a03c6a9dcb51a073c53e602fd5dbdd9479186d
Jakaria Hossin <jakariahossinsv@gmail.com>
2025-06-26 09:58:10 +0600

    Create 3.4.c

---
+++ 3.4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double t;
+    printf("Enter an angle in degrees: ");
+    scanf("%lf", &t);
+
+    double b = tan(t * M_PI / 180.0);  // Convert to radians
+    printf("tan(%.2lf) = %.2lf\n", t, b);
+
+    return 0;
+}
