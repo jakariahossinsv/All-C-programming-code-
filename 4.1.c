d17c22da3a76c541f4234dbed455394e19dee120
Jakaria Hossin <jakariahossinsv@gmail.com>
2025-06-27 20:14:53 +0600

    Create 4.1.c

---
+++ 4.1.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main(){
+  int a;
+  printf("enter the number :");
+  scanf("%d",&a);
+  if(a%2!=0){
+    printf("the number is odd");
+  }
+  else{
+    printf("the number is even");
+  }
+  return 0;
+}
