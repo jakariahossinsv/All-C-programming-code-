89eea7d1d594ba71a95fe30919d27a2d85ee0251
Jakaria Hossin <jakariahossinsv@gmail.com>
2025-06-27 20:19:11 +0600

    Create 4.6.c

---
+++ 4.6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int a,b,c;
+  printf("enter the first number :");
+  scanf("%d",&a);
+  printf("enter the second number :");
+  scanf("%d",&b);
+  printf("enter the third number :");
+  scanf("%d",&c);
+  if(a>b && a>c){
+    printf("the greatest number is %d",a);
+  }
+  else if(b>a && b>c){
+    printf("the greatest number is %d",b);
+  }
+  else {
+    printf("the greatest number is %d",c);
+  }
+  return 0;
+}
